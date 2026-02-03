29505d227566c00cdafa3cd4004a1b5e4d54ff33
dan-acsc <dan-acsc@users.noreply.github.com>
2026-02-03 02:44:28 +0000

    feat: remove black

--- azul_plugin_email/parser.py
+++ azul_plugin_email/parser.py
@@ -180,8 +180,8 @@ class Attachment:
         # Otherwise just make something up!
         if filename is None:
             r = "".join(
-                random.choice(string.ascii_uppercase + string.digits)
-                for _ in range(5)  # noqa: S311 # nosec B311
+                random.choice(string.ascii_uppercase + string.digits)  # noqa: S311
+                for _ in range(5)
             )
             filename = "UnknownFilename " + r + ".bin"
         # ensure there's no shenanigans with path chars
